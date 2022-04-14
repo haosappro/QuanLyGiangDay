c3304cbec2cb02bc319931379b9d2359994b7906
haosappro <haop7078@gmail.com>
2022-04-14 23:55:53 +0700

    first commit

---
+++ QuanLyGiangDay/QuanLyGiangDay/Controllers/GiangViensController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyGiangDay.Models;
+
+namespace QuanLyGiangDay.Controllers
+{
+    public class GiangViensController : Controller
+    {
+        private TechDBEntities db = new TechDBEntities();
+
+        // GET: GiangViens
+        public ActionResult Index()
+        {
+            return View(db.GiangViens.ToList());
+        }
+
+        // GET: GiangViens/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiangVien giangVien = db.GiangViens.Find(id);
+            if (giangVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giangVien);
+        }
+
+        // GET: GiangViens/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GiangViens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaGV,HotenGiangVien,NgayThangNamSinh,DanToc,QueQuan,QuocTich,CMND,DienThoai,Email")] GiangVien giangVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GiangViens.Add(giangVien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(giangVien);
+        }
+
+        // GET: GiangViens/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiangVien giangVien = db.GiangViens.Find(id);
+            if (giangVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giangVien);
+        }
+
+        // POST: GiangViens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaGV,HotenGiangVien,NgayThangNamSinh,DanToc,QueQuan,QuocTich,CMND,DienThoai,Email")] GiangVien giangVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(giangVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(giangVien);
+        }
+
+        // GET: GiangViens/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiangVien giangVien = db.GiangViens.Find(id);
+            if (giangVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giangVien);
+        }
+
+        // POST: GiangViens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            GiangVien giangVien = db.GiangViens.Find(id);
+            db.GiangViens.Remove(giangVien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
